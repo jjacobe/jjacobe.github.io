edda1c45484e9f667291367e335ecccfe6e64ec2
jjacobe <jac19002@byui.edu>
2021-10-08 14:29:10 -0600

    commit

---
+++ lesson4/js/preston.js
@@ -0,0 +1,41 @@
+
+// Function for toggling navigation menu.
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide")
+}
+// Create formating for last updated information
+var weekday = new Array(
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+    );
+var months = new Array(
+    "January", 
+    "February", 
+    "March",
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September",
+    "October", 
+    "November", 
+    "December"
+    );
+const year = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+var day = lastModified.getDay();
+var day = weekday[day];
+var month = lastModified.getMonth();
+var month = months[month];
+
+var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+
+// Display copyright year and date last modified to footer of HTML document.
+document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
+document.getElementById("copyrightYear").textContent = year;
