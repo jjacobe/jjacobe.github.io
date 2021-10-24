410921ae08e85e5c846e5edccd601d368bf5f881
jjacobe <jac19002@byui.edu>
2021-10-24 17:14:35 -0600

    complete update

--- lesson6/js/preston.js
+++ lesson6/js/preston.js
@@ -1,58 +1,39 @@
-// Toggle nav menu
+// Date function
+let weekday = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+let d = new Date();
+let dayName = weekday[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + d.getDate() + " " + monthName + " "  + d.getFullYear();
+document.getElementById("dateLastMod").textContent = fullDate;
+
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+// Menu
 function toggleMenu() {
-    document.getElementById("navBar").classList.toggle("hide")
+    document.getElementById("menu").classList.toggle("navigation")
 }
-// Create formating for last updated information
-var weekdays = new Array(
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    );
-var months = new Array(
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-    );
-const year = new Date().getFullYear();
-const lastModified = new Date(document.lastModified);
-var day = lastModified.getDay();
-var day = weekdays[day];
-var month = lastModified.getMonth();
-var month = months[month];
 
-var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
-
-// Display copyright year and date last modified
-document.getElementById("copyrightYear").textContent = year;
-document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
-
-// Friday pancake banner
-function createBanner(day) {
-    if (day == "Friday") {
-        document.querySelector("#banner").style.display = "block";
-    }
+// Pancake banner
+let pancakeDay = new Date();
+if (pancakeDay.getDay() == 5) {
+   document.querySelector('#pancake').getElementsByClassName.display = 'flex';
 }
-createBanner(day);
 
 // Wind chill function
 let temp = document.querySelector('#curTemp').innerText;
 let speed = document.querySelector('#curSpeed').innerText;
-
 buildWC(speed, temp);
-
-// Calculate the Windchill
 function buildWC(speed, temp) {
     let wcTemp = document.getElementById('windchill');
-   
-    // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log('windchill 01: ' + wc);
-   
-    // Round the answer down to integer
     wc = Math.floor(wc);
     console.log('windchill 02: ' + wc);
     console.log(temp);
-    // If chill is greater than temp, return the temp
     wc = (wc > temp) ? temp : wc;
     console.log('windchill 03: ' + wc);
-   
-    // Display the windchill
     console.log('windchill 04: ' + wc);
     wc = wc+'°F';
     wcTemp.innerHTML = wc;
